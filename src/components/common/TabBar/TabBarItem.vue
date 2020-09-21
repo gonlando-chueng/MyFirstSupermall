1b564765204ef5f2df62b1023ee04f3b64933288
gonlando-chueng <986648933@qq.com>
2020-09-21 22:37:39 +0800

    添加tabbar

---
+++ src/components/common/TabBar/TabBarItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="tab-bar-item" @click='itemClick'>
+    <div :class='{active:isActive}'><slot name='item-icon'></slot></div>
+    <div :class='{active:isActive}'><slot name='item-text'></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TabBarItem',
+  props:{
+    path:String,
+  },
+  computed:{
+    isActive(){
+      return this.$route.path.indexOf(this.path) !== -1
+    }
+  },
+  methods:{
+    itemClick(){
+      this.$router.push(this.path).catch(() =>{})
+    }
+  }
+}
+</script>
+
+<style>
+@import "../../../assets/css/TabBarIcon/iconfont.css";
+.tab-bar-item{
+  flex:1;
+  text-align:center;
+  height:49px;
+  position:relative;
+}
+.tab-bar-item div>p{
+  width:24px;
+  height:24px;
+  font-size:24px;
+  left:50%;
+  transform:translateX(-50%);
+  position:absolute;
+  top:-34px;
+}
+.tab-bar-item>div div{
+  position:absolute;
+  left:50%;
+  transform:translateX(-50%);
+  top:12px;
+}
+.active{
+  color:#ff6700;
+}
+</style>
---
+++ src/components/content/MainTabBar/MainTabBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <tab-bar>
+      <tab-bar-item path='/home'>
+        <p class='iconfont icon-home1' slot='item-icon'></p>
+        <div slot='item-text'>首页</div>
+      </tab-bar-item>
+      <tab-bar-item path='/category'>
+        <p class='iconfont icon-leimupinleifenleileibie2' slot='item-icon'></p>
+        <div slot='item-text'>分类</div>
+      </tab-bar-item>
+      <tab-bar-item path='/cart'>
+        <p class='iconfont icon-gouwuchekong' slot='item-icon'></p>
+        <div slot='item-text'>购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path='/profile'>
+        <p class='iconfont icon-wodedangxuan' slot='item-icon'></p>
+        <div slot='item-text'>我的</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+import TabBar from 'components/common/TabBar/TabBar'
+import TabBarItem from 'components/common/TabBar/TabBarItem'
+export default {
+  name:'MainTabBar',
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
